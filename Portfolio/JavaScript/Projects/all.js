aea78789d49b93b0675113c5914616ead868d326
joowus <joonaowusu@gmail.com>
2026-02-12 14:13:01 +0200

    asdads

---
+++ Portfolio/JavaScript/Projects/all.js
@@ -0,0 +1,15 @@
+function loadAllProjects() {
+
+    innerHTML = `
+        <div class="project-card">
+            <h3 class="project-title">Project One</h3>
+            <div>
+                <img src="/Portfolio/Images/Test/project1.png" alt="Project One Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A web application that allows users to track their tasks and manage their time effectively.</p>
+                    <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
+                </div>
+            </div>
+        </div>
+    `;
+}
