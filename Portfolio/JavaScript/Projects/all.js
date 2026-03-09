c1496ec3a63cdead266639d0c66c468fb7d7334c
joowus <joonaowusu@gmail.com>
2026-03-09 10:10:28 +0200

    fwefw

--- Portfolio/JavaScript/Projects/all.js
+++ Portfolio/JavaScript/Projects/all.js
@@ -1,6 +1,17 @@
 function loadAllProjects() {
 
-    innerHTML = `
+    projectsGrid.innerHTML = `
+        <div class="project-card">
+            <h3 class="project-title">Dark Mode Toggle</h3>
+            <div>
+                <img src="/Portfolio/Images/100-Projects/darkmode.png" alt="Dark Mode Toggle Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A simple dark mode toggle for a web application.</p>
+                    <a class="project-link" href="http://127.0.0.1:5501/Vscode-Stuff/100-Projects/Finished/Project-Dark-Mode-Toggle/index.html">View Project</a>
+                </div>
+            </div>
+        </div>
+
         <div class="project-card">
             <h3 class="project-title">Project One</h3>
             <div>
@@ -11,5 +22,50 @@ function loadAllProjects() {
                 </div>
             </div>
         </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Project One</h3>
+            <div>
+                <img src="/Portfolio/Images/Test/project1.png" alt="Project One Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A web application that allows users to track their tasks and manage their time effectively.</p>
+                    <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Project One</h3>
+            <div>
+                <img src="/Portfolio/Images/Test/project1.png" alt="Project One Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A web application that allows users to track their tasks and manage their time effectively.</p>
+                    <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Project One</h3>
+            <div>
+                <img src="/Portfolio/Images/Test/project1.png" alt="Project One Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A web application that allows users to track their tasks and manage their time effectively.</p>
+                    <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Project One</h3>
+            <div>
+                <img src="/Portfolio/Images/Test/project1.png" alt="Project One Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A web application that allows users to track their tasks and manage their time effectively.</p>
+                    <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
+                </div>
+            </div>
+        </div>
+
     `;
 }
