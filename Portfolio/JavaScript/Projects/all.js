9f3250bc46d840bab1a64253b215d521664c090e
joowus <joonaowusu@gmail.com>
2026-03-11 15:00:35 +0200

    ergerg

--- Portfolio/JavaScript/Projects/all.js
+++ Portfolio/JavaScript/Projects/all.js
@@ -1,67 +1,145 @@
 function loadAllProjects() {
 
     projectsGrid.innerHTML = `
+
         <div class="project-card">
             <h3 class="project-title">Dark Mode Toggle</h3>
             <div>
                 <img src="/Portfolio/Images/100-Projects/darkmode.png" alt="Dark Mode Toggle Screenshot" class="project-image">
                 <div class="project-desc-container">
                     <p class="project-description">A simple dark mode toggle for a web application.</p>
-                    <a class="project-link" href="http://Vscode-Stuff/100-Projects/Finished/Project-Dark-Mode-Toggle/index.html">View Project</a>
+                    <a class="project-link" href="">View Project</a>
                 </div>
             </div>
         </div>
 
         <div class="project-card">
-            <h3 class="project-title">Project One</h3>
+            <h3 class="project-title">Dice Roller</h3>
             <div>
-                <img src="/Portfolio/Images/Test/project1.png" alt="Project One Screenshot" class="project-image">
+                <img src="/Portfolio/Images/100-Projects/diceroll.png" alt="Dice Roller Screenshot" class="project-image">
                 <div class="project-desc-container">
-                    <p class="project-description">A web application that allows users to track their tasks and manage their time effectively.</p>
+                    <p class="project-description">A simple dice roller game where users can roll a virtual dice.</p>
+                    <a class="project-link" href="">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Heart Trail Animation</h3>
+            <div>
+                <img src="/Portfolio/Images/100-Projects/hearttrail.png" alt="Heart Trail Animation Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A simple heart trail animation for a web application.</p>
                     <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
                 </div>
             </div>
         </div>
 
         <div class="project-card">
-            <h3 class="project-title">Project One</h3>
+            <h3 class="project-title">Image Slider</h3>
             <div>
-                <img src="/Portfolio/Images/Test/project1.png" alt="Project One Screenshot" class="project-image">
+                <img src="/Portfolio/Images/100-Projects/imgslider.png" alt="Image Slider Screenshot" class="project-image">
                 <div class="project-desc-container">
-                    <p class="project-description">A web application that allows users to track their tasks and manage their time effectively.</p>
+                    <p class="project-description">A simple image slider for a web application.</p>
                     <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
                 </div>
             </div>
         </div>
 
         <div class="project-card">
-            <h3 class="project-title">Project One</h3>
+            <h3 class="project-title">Pomodoro Timer</h3>
             <div>
-                <img src="/Portfolio/Images/Test/project1.png" alt="Project One Screenshot" class="project-image">
+                <img src="/Portfolio/Images/100-Projects/pomodoro.png" alt="Pomodoro Timer Screenshot" class="project-image">
                 <div class="project-desc-container">
-                    <p class="project-description">A web application that allows users to track their tasks and manage their time effectively.</p>
+                    <p class="project-description">A simple Pomodoro timer where users can set work and break intervals.</p>
                     <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
                 </div>
             </div>
         </div>
 
         <div class="project-card">
-            <h3 class="project-title">Project One</h3>
+            <h3 class="project-title">Recipe Book App</h3>
             <div>
-                <img src="/Portfolio/Images/Test/project1.png" alt="Project One Screenshot" class="project-image">
+                <img src="/Portfolio/Images/100-Projects/recipebookapp.png" alt="Recipe Book App Screenshot" class="project-image">
                 <div class="project-desc-container">
-                    <p class="project-description">A web application that allows users to track their tasks and manage their time effectively.</p>
+                    <p class="project-description">A simple recipe book app that generates random recipes for the user.</p>
                     <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
                 </div>
             </div>
         </div>
 
         <div class="project-card">
-            <h3 class="project-title">Project One</h3>
+            <h3 class="project-title">Sidebar</h3>
             <div>
-                <img src="/Portfolio/Images/Test/project1.png" alt="Project One Screenshot" class="project-image">
+                <img src="/Portfolio/Images/100-Projects/sidebar.png" alt="Sidebar Screenshot" class="project-image">
                 <div class="project-desc-container">
-                    <p class="project-description">A web application that allows users to track their tasks and manage their time effectively.</p>
+                    <p class="project-description">A simple sidebar for a web application.</p>
+                    <a class="project-link" href="">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Stopwatch</h3>
+            <div>
+                <img src="/Portfolio/Images/100-Projects/stopwatch.png" alt="Stopwatch Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A simple stopwatch where users can start, stop, and reset the timer.</p>
+                    <a class="project-link" href="">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Tabs Section</h3>
+            <div>
+                <img src="/Portfolio/Images/100-Projects/tabs.png" alt="Tabs Section Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A simple tabs section for a web application.</p>
+                    <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Temperature Converter</h3>
+            <div>
+                <img src="/Portfolio/Images/100-Projects/tempconverter.png" alt="Temperature Converter Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A simple temperature converter where users can convert between Celsius, Fahrenheit and Kelvin.</p>
+                    <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Testimonial Slider</h3>
+            <div>
+                <img src="/Portfolio/Images/100-Projects/testimonialslider.png" alt="Testimonial Slider Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A simple testimonial slider for a web application.</p>
+                    <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Tip Calculator</h3>
+            <div>
+                <img src="/Portfolio/Images/100-Projects/tipcalculator.png" alt="Tip Calculator Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A simple tip calculator for a web application.</p>
+                    <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Video Trailer Popup</h3>
+            <div>
+                <img src="/Portfolio/Images/100-Projects/videotrailer.png" alt="Video Trailer Popup Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">A simple video trailer popup where users can view a video trailer for a web application.</p>
                     <a class="project-link" href="/Portfolio/Projects/Project1.html">View Project</a>
                 </div>
             </div>
@@ -69,3 +147,5 @@ function loadAllProjects() {
 
     `;
 }
+
+export { loadAllProjects };
---
+++ Portfolio/JavaScript/script.js
@@ -0,0 +1,63 @@
+import { loadAllProjects } from './Projects/all.js';
+
+const allProjectsBtn = document.getElementById('allProjectsBtn');
+const beginnerProjectsBtn = document.getElementById('beginnerProjectsBtn');
+const _100ProjectsBtn = document.getElementById('100ProjectsBtn');
+const courseProjectsBtn = document.getElementById('courseProjectsBtn');
+const SPRINTProjectsBtn = document.getElementById('SPRINTProjectsBtn');
+const otherProjectsBtn = document.getElementById('otherProjectsBtn');
+
+const projectsExplanation = document.getElementById('projectCategoryExplanation');
+
+function clearActive() {
+    allProjectsBtn.classList.remove('project-category-active');
+    beginnerProjectsBtn.classList.remove('project-category-active');
+    _100ProjectsBtn.classList.remove('project-category-active');
+    courseProjectsBtn.classList.remove('project-category-active');
+    SPRINTProjectsBtn.classList.remove('project-category-active');
+    otherProjectsBtn.classList.remove('project-category-active');
+}
+
+window.onload = () => {
+    clearActive();
+    allProjectsBtn.classList.add('project-category-active');
+    projectsExplanation.textContent = "A collection of all my projects, showcasing a wide range of skills and technologies.";
+    loadAllProjects();
+}
+
+allProjectsBtn.addEventListener('click', () => {
+    clearActive();
+    allProjectsBtn.classList.add('project-category-active');
+    projectsExplanation.textContent = "A collection of all my projects, showcasing a wide range of skills and technologies.";
+    loadAllProjects();
+});
+beginnerProjectsBtn.addEventListener('click', () => {
+    clearActive();
+    beginnerProjectsBtn.classList.add('project-category-active');
+    projectsExplanation.textContent = "Simple projects that helped me learn the basics of programming and web development.";
+    loadAllProjects();
+});
+_100ProjectsBtn.addEventListener('click', () => {
+    clearActive();
+    _100ProjectsBtn.classList.add('project-category-active');
+    projectsExplanation.textContent = "A series of 100 small projects that I completed to practice and improve my coding skills.";
+    loadAllProjects();
+});
+courseProjectsBtn.addEventListener('click', () => {
+    clearActive();
+    courseProjectsBtn.classList.add('project-category-active');
+    projectsExplanation.textContent = "Projects that I completed as part of my coursework, demonstrating my ability to apply what I've learned in a structured environment.";
+    loadAllProjects();
+});
+SPRINTProjectsBtn.addEventListener('click', () => {
+    clearActive();
+    SPRINTProjectsBtn.classList.add('project-category-active');
+    projectsExplanation.textContent = "Projects that I completed during intensive coding sprints, showcasing my ability to work under pressure and deliver results quickly.";
+    loadAllProjects();
+});
+otherProjectsBtn.addEventListener('click', () => {
+    clearActive();
+    otherProjectsBtn.classList.add('project-category-active');
+    projectsExplanation.textContent = "A variety of projects that don't fit into the other categories but still demonstrate my skills and creativity.";
+    loadAllProjects();
+});
