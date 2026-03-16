e63d568b3b7673b6d2f9bb7d91ee75d18bccd592
joowus <joonaowusu@gmail.com>
2026-03-16 10:16:02 +0200

    sdvsv

--- Portfolio/JavaScript/Projects/course.js
+++ Portfolio/JavaScript/Projects/course.js
@@ -5,15 +5,116 @@ function loadBeginnerProjects() {
     projectsGrid.innerHTML = `
 
         <div class="project-card">
-            <h3 class="project-title">First Project</h3>
+            <h3 class="project-title">Java Practice</h3>
             <div>
-                <img src="/Portfolio/Images/Beginner/first.png" alt="First Project Screenshot" class="project-image">
+                <img src="/Portfolio/Images/Beginner/javapractice.png" alt="Java Practice Screenshot" class="project-image">
                 <div class="project-desc-container">
-                    <p class="project-description">My first web development project where I learned the basics of HTML.</p>
+                    <p class="project-description">Starting to learn Javascript.</p>
                     <a class="project-link" href="">View Project</a>
                 </div>
             </div>
         </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Variables</h3>
+            <div>
+                <img src="/Portfolio/Images/Beginner/variables.png" alt="Variables Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">Learning about variables in Java.</p>
+                    <a class="project-link" href="">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Cart Quantity</h3>
+            <div>
+                <img src="/Portfolio/Images/Beginner/cart.png" alt="Cart Quantity Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">Learning more about Java programming by learning button onClick events.</p>
+                    <a class="project-link" href="">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">True or False</h3>
+            <div>
+                <img src="/Portfolio/Images/Beginner/truefalse.png" alt="True or False Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">Learning about boolean values in Java.</p>
+                    <a class="project-link" href="">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Tax</h3>
+            <div>
+                <img src="/Portfolio/Images/Beginner/tax.png" alt="Tax Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">Learning about functions and creating small tax calculations.</p>
+                    <a class="project-link" href="">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Constants and Methods</h3>
+            <div>
+                <img src="/Portfolio/Images/Beginner/constmethod.png" alt="Constants and Methods Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">Learning about constants and methods in Java.</p>
+                    <a class="project-link" href="">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Buttons and innerHTML</h3>
+            <div>
+                <img src="/Portfolio/Images/Beginner/buttons.png" alt="Buttons Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">Learning more about button onClick events and innerHTML in Java.</p>
+                    <a class="project-link" href="">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Youtube and Shipping</h3>
+            <div>
+                <img src="/Portfolio/Images/Beginner/youtubeandshipping.png" alt="Youtube and Shipping Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">Creating a working youtube subscription button and a shipping calculator.</p>
+                    <a class="project-link" href="">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">Youtube and Shipping</h3>
+            <div>
+                <img src="/Portfolio/Images/Beginner/youtubeandshipping.png" alt="Youtube and Shipping Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">Creating a working youtube subscription button and a shipping calculator.</p>
+                    <a class="project-link" href="">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+            <h3 class="project-title">RAW Rock Paper Scissors</h3>
+            <div>
+                <img src="/Portfolio/Images/Beginner/rockpaperscissors.png" alt="RAW Rock Paper Scissors Screenshot" class="project-image">
+                <div class="project-desc-container">
+                    <p class="project-description">Creating a simple Rock Paper Scissors game.</p>
+                    <a class="project-link" href="">View Project</a>
+                </div>
+            </div>
+        </div>
+
+        
 
     `;
 }
